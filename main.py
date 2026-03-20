eb625215e916fd9c555107d046eab79599f3b4ea
Shrawan karki <pratyushkarki6@gmail.com>
2026-03-20 16:23:31 +0545

    changed
    made the code clean

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import user
 users = {
     "Test1":   {"password": "pass1", "balance": 500000, "banned": False},
     "Test2": {"password": "pass2", "balance": 500000, "banned": False},
-    "Test3":   {"password": "pass3", "balance": 500000, "banned": False}
+    "Test3":   {"password": "pass3", "balance": 500000, "banned": False},
+    "Test4":   {"password": "pass4", "balance": 500000, "banned": False}
 }
 while True:
-
+        print("---------------------")
         print("1: Admin")
         print("2: User")
         print("---------------------")
